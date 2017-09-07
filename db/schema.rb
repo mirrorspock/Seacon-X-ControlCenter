bc9a80b5c973c4289c2f62659f7ab419c24b8a85
Kai-Arne Reiter <git@mirrorspock.com>
2017-09-07 08:55:59 +0200

    heroku update

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170907062332) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activators", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
